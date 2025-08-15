4b48ca00a38450f67e0c008135892610e6e04d30
wardDward <edward.taligatos03@gmail.com>
2025-08-15 21:08:18 +0800

    feat: sidebar responsiveness

--- src/app/(learners)/layout.tsx
+++ src/app/(learners)/layout.tsx
@@ -1,83 +1,87 @@
-"use client"
-import React, { ReactNode, useState } from 'react'
-import Sidebar from '../_components/Sidebar';
-import Navbar from './_components/Navbar';
+"use client";
+import React, { ReactNode, useState } from "react";
+import Sidebar from "../_components/Sidebar";
+import Navbar from "./_components/Navbar";
 import { MdDashboard } from "react-icons/md";
 import { FaBook } from "react-icons/fa";
-import { MdExplore } from "react-icons/md";
-import { MdForum } from "react-icons/md";
+import { MdExplore, MdForum } from "react-icons/md";
 import { FaGears } from "react-icons/fa6";
-import { IoPersonCircle } from "react-icons/io5";
-import { IoLogOut } from "react-icons/io5";
-import { Links } from '../interface/Links';
+import { IoPersonCircle, IoLogOut } from "react-icons/io5";
+import { Links } from "../interface/Links";
 
 interface LayoutProps {
-    children: ReactNode
+  children: ReactNode;
 }
+
 function Layout({ children }: LayoutProps) {
-    const [links, setLinks] = useState<Links[]>([
+  const [links] = useState<Links[]>([
+    {
+      name: "Dashboard",
+      path: "/dashboard",
+      icon: <MdDashboard size={20} />,
+      type: "link",
+    },
+    {
+      name: "Courses",
+      path: "/courses",
+      icon: <FaBook size={20} />,
+      type: "link",
+    },
+    {
+      name: "Browse",
+      path: "/browses",
+      icon: <MdExplore size={20} />,
+      type: "link",
+    },
+    {
+      name: "Forum",
+      path: "/forums",
+      icon: <MdForum size={20} />,
+      type: "link",
+    },
+    {
+      name: "Settings",
+      path: "#",
+      icon: <FaGears size={20} />,
+      type: "group",
+      items: [
         {
-            name: "Dashboard",
-            path: "/dashboard",
-            icon: <MdDashboard size={20} />,
-            type: "link",
+          name: "Profile",
+          path: "/profile",
+          icon: <IoPersonCircle size={20} />,
+          type: "link",
         },
         {
-            name: "Courses",
-            path: "/courses",
-            icon: <FaBook size={20} />,
-            type: "link",
+          name: "Logout",
+          path: "/logout",
+          icon: <IoLogOut size={20} />,
+          type: "link",
         },
-        {
-            name: "Browse",
-            path: "/browses",
-            icon: <MdExplore size={20} />,
-            type: "link",
-        },
-        {
-            name: "Forum",
-            path: "/forums",
-            icon: <MdForum size={20} />,
-            type: "link",
-        },
-        {
-            name: "Settings",
-            path: "#",
-            icon: <FaGears size={20} />,
-            type: "group",
-            items: [
-                {
-                    name: "Profile",
-                    path: "/profile",
-                    icon: <IoPersonCircle size={20} />,
-                    type: "link",
-                },
-                {
-                    name: "Logout",
-                    path: "/logout",
-                    icon: <IoLogOut size={20} />,
-                    type: "link",
-                },
-            ],
-        },
-    ]);
-    const [toggle, setToggle] = useState<boolean>(true)
+      ],
+    },
+  ]);
 
-    const sendData = () => {
-        setToggle((prev) => !prev)
-        console.log(toggle)
-   }
-    return (
-        <section className="flex">
-            <Sidebar links={links} toggle={toggle} />
-            <div className={`flex flex-col  w-full ${toggle ? ' md:w-[calc(100%-180px)] lg:w-[calc(100%-250px)] ml-0 md:ml-[180px] lg:ml-[250px]' 
-                : 'md:w-[calc(100%-60px)] md:ml-[60px]'
-                 }`}>
-                <Navbar toggle={toggle} onToggle={sendData}/>
-                {children}
-            </div>
-        </section>
-    )
+  const [toggle, setToggle] = useState<boolean>(false);
+
+  const sendData = () => {
+    setToggle((prev) => !prev);
+  };
+
+  return (
+    <section className="flex">
+      <Sidebar links={links} toggle={toggle} onToggle={sendData} />
+      <div
+        className={`flex flex-col w-full transition-all duration-300 ${
+          toggle
+            ? "md:w-[calc(100%-180px)] lg:w-[calc(100%-250px)] md:ml-[180px] lg:ml-[250px]"
+            : "md:w-[calc(100%-60px)] md:ml-[60px]"
+        }`}
+      >
+        <Navbar toggle={toggle} onToggle={sendData} />
+        {children}
+      </div>
+    </section>
+  );
 }
 
-export default Layout
+export default Layout;
--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -1,33 +1,56 @@
 import Link from "next/link";
-import Image from 'next/image'
-import React, { useState } from "react";
+import Image from "next/image";
+import React, { useRef, useState } from "react";
 import { IoChevronDownOutline } from "react-icons/io5";
 import { Links } from "../interface/Links";
-
+import useClickOutside from "../_hooks/useClickOutside";
 
 export interface props {
   links: Links[];
   toggle: boolean;
   onToggle: () => void;
 }
+
 function Sidebar({ links, toggle, onToggle }: props) {
   const [isOpen, setIsOpen] = useState<any>({});
+  const sidebar = useRef<HTMLDivElement>(null)
   const openGroup = (linkName: string) => {
     setIsOpen((prev: any) => ({
       ...prev,
       [linkName]: !prev[linkName],
     }));
   };
- 
+
+  //for link closing
+  const handleLinkClick = () => {
+    if (window.innerWidth < 768) {
+      onToggle();
+    }
+  };
+
+  useClickOutside(sidebar, onToggle)
+
   return (
-    <div className={`bg-white border-r-[1px] border-slate-200 fixed inset-y-0 hidden md:flex flex-col ${toggle ? 
-    'w-[180px] lg:w-[250px]' : 'w-[60px]'} p-[10px] z-[99999]`}>
+    //translate condition is for sm/mobile reso hidding the sidebar
+    <div
+      ref={sidebar}
+      className={`bg-white border-r border-slate-200 fixed inset-y-0 flex flex-col
+        transform transition-transform duration-300 ease-in-out z-[99999]
+        ${toggle ? "translate-x-0" : "-translate-x-full"} 
+        md:translate-x-0
+        ${toggle ? "w-[250px]" : "w-[60px]"}
+        p-[10px]`}
+    >
       <div className="flex items-center">
         <h2 className="mb-2 pt-[10px] flex items-center">
-           <div className="relative h-[40px] w-[40px]">
-              <Image fill src="/icon.png" alt=""/>
-            </div>
-          {toggle ? (<span className="text-purpleIndigo text-xl lg:text-lg tracking-wider font-[500]">Educore</span>) : ''}
+          <div className="relative h-[40px] w-[40px]">
+            <Image fill src="/icon.png" alt="" />
+          </div>
+          {toggle && (
+            <span className="text-purpleIndigo text-xl lg:text-lg tracking-wider font-[500]">
+              Educore 
+            </span>
+          )}
         </h2>
       </div>
 
@@ -36,11 +59,12 @@ function Sidebar({ links, toggle, onToggle }: props) {
           link.type === "link" ? (
             <Link
               href={link.path}
+              onClick={handleLinkClick}
               className="text-black flex items-center text-sm lg:text-md p-2 hover:text-white hover:bg-purpleIndigo rounded-md w-full"
               key={link.name}
             >
               <span className="mr-2">{link.icon}</span>
-              {toggle ? link.name : '' }
+              {toggle ? link.name : ""}
             </Link>
           ) : (
             <React.Fragment key={link.name}>
@@ -50,24 +74,28 @@ function Sidebar({ links, toggle, onToggle }: props) {
               >
                 <div className="flex items-center">
                   <span className="mr-2">{link.icon}</span>
-                  {toggle ? link.name : ''}
+                  {toggle ? link.name : ""}
                 </div>
-                {toggle ? ( <IoChevronDownOutline
-                  className={`transition-transform ${isOpen[link.name] ? "rotate-180" : ""
+                {toggle && (
+                  <IoChevronDownOutline
+                    className={`transition-transform ${
+                      isOpen[link.name] ? "rotate-180" : ""
                     }`}
-                  size={20}
-                />) : ''}
+                    size={20}
+                  />
+                )}
               </div>
               {isOpen[link.name] && (
-                <div className={`${toggle ? 'pl-3' : 'pl-0'} flex flex-col`}>
+                <div className={`${toggle ? "pl-3" : "pl-0"} flex flex-col`}>
                   {link.items.map((l: any) => (
                     <Link
                       href={l.path}
+                      onClick={handleLinkClick}
                       key={l.name}
                       className="text-black flex items-center text-sm lg:text-md p-2 hover:text-white hover:bg-purpleIndigo rounded-md w-full"
                     >
                       <span className="mr-2">{l.icon}</span>
-                       {toggle ? l.name : ''}
+                      {toggle ? l.name : ""}
                     </Link>
                   ))}
                 </div>
