ffb77ef61a09a30f3fe8fea5acd8fdee6172be38
wardDward <edward.taligatos03@gmail.com>
2025-08-18 00:28:38 +0800

    WIP: login integration

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,30 @@
+'use client'
+import { useLoginMutation } from '@/app/store/services/authServices'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useState } from 'react'
+import { handleInput } from '../../utils/handleInput';
 
+interface FormType {
+    email: string;
+    password: string;
+}
 function Home() {
+    const [formData, setFormData] = useState<FormType>({
+        email: '',
+        password: ''
+    })
+
+    const inputState = (e: React.ChangeEvent<HTMLInputElement>) => {
+        handleInput(e, formData, setFormData)
+    }
+
+    const [login, { data, isLoading }] = useLoginMutation()
+
+    const handleSubmit = async () => {
+        await login(formData)
+    }
+
     return (
         <section className='flex justify-center items-center h-screen bg-gradient-to-r from-purpleIndigo to-darkPurpleIndigo'>
             <div className='bg-white w-full md:w-[50%] lg:w-[30%] xl:w-[25%] px-2 py-5 shadow-lg rounded-md border-[1px] border-gray-100'>
@@ -19,12 +42,14 @@ function Home() {
                 <div className='mt-4 px-2'>
                     <form action="">
                         <div className='flex flex-col mb-3'>
-                            <label htmlFor="email" className='text-md'>Email</label>
-                            <input type="email" name="email" id="email" className='w-full border-[1px] border-gray-400 py-1 rounded-md outline-none px-2' />
+                            <label htmlFor="email" className='text-md'>Email                             {formData.email}
+                            </label>
+                            <input type="email" onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                inputState(e)} name="email" id="email" className='w-full border-[1px] border-gray-400 py-1 rounded-md outline-none px-2' />
                         </div>
                         <div className='flex flex-col mb-3'>
                             <label htmlFor="password" className='text-md'>Password</label>
-                            <input type="password" name="password" id="password" className='w-full border-[1px] border-gray-400 py-1 rounded-md outline-none px-2' />
+                            <input type="password" onChange={(e: React.ChangeEvent<HTMLInputElement>) => inputState(e)} name="password" id="password" className='w-full border-[1px] border-gray-400 py-1 rounded-md outline-none px-2' />
                         </div>
                         <div className='mt-3'>
                             <button type="submit" className='text-md w-full bg-purpleIndigo hover:bg-darkPurpleIndigo cursor-pointer py-1 text-white tracking-wide rounded-md'>Sign In</button>
