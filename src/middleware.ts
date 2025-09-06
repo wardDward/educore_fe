efff240401eb273a926858c98a2b389cc4738da3
wardDward <edward.taligatos03@gmail.com>
2025-09-06 15:54:37 +0800

    feat: runtime type

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,18 @@
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { checkAuth } from "./app/middleware/authMiddleware";
 
-
 export function middleware(req: NextRequest) {
-    return checkAuth(req)
+  return checkAuth(req);
 }
+
 export const config = {
   matcher: [
-    "/",                   // root
-    "/dashboard/:path*",   // learner pages
-    "/profile/:path*",     // learner pages
-    "/instructor/:path*",  // instructor pages
-    "/login",              // so middleware can redirect logged-in users
-    "/register",           // same
+    "/",             
+    "/login",
+    "/register",
+    "/dashboard/:path*",
+    "/profile/:path*",
+    "/instructor/:path*",
   ],
+  runtime: "edge",     
 };
